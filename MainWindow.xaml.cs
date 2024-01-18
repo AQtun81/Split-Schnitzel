c35d9fc76f327e00e0769363c72eaec22ab052e6
AQtun81 <Lysy9990@Gmail.com>
2024-01-18 01:39:36 +0100

    Code cleanup, moved code related to managing external windows into ManagedWindow.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Runtime.InteropServices;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Media;
 using HandyControl.Tools;
 
@@ -20,43 +19,16 @@ public partial class MainWindow : Window
     // Window Manipulation Methods
     [DllImport("user32.dll")]
     static extern IntPtr GetForegroundWindow();
-    
-    [DllImport("user32.dll")]
-    public static extern IntPtr SetWindowPos(IntPtr hWnd, WindowInsertAfter hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
-    
-    [DllImport("user32.dll")]
-    public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
-    public enum WindowInsertAfter : int
-    {
-        HWND_TOPMOST = -1,
-        HWND_TOP = 0,
-        HWND_BOTTOM = 1,
-        HWND_NOTOPMOST = -2
-    }
-
-    public enum WindowFlags : int
-    {
-        SWP_ASYNCWINDOWPOS = 0x4000,
-        SWP_DEFERERASE = 0x2000,
-        SWP_DRAWFRAME = 0x0020,
-        SWP_FRAMECHANGED = 0x0020,
-        SWP_HIDEWINDOW = 0x0080,
-        SWP_NOACTIVATE = 0x0010,
-        SWP_NOCOPYBITS = 0x0100,
-        SWP_NOMOVE = 0x0002,
-        SWP_NOOWNERZORDER = 0x0200,
-        SWP_NOREDRAW = 0x0008,
-        SWP_NOREPOSITION = 0x0200,
-        SWP_NOSENDCHANGING = 0x0400,
-        SWP_NOSIZE = 0x0001,
-        SWP_NOZORDER = 0x0004,
-        SWP_SHOWWINDOW = 0x0040,
-    }
-
+    // window handles
     private static IntPtr? thisWindowHandle;
-    private static IntPtr? managedLeftWindow;
-    private static IntPtr? managedRightWindow;
+    private static ManagedWindow?[] managedWindows = new ManagedWindow?[2];
+
+    private enum WindowSlot : byte
+    {
+        Left,
+        Right
+    }
     
     public MainWindow()
     {
@@ -70,30 +42,13 @@ public partial class MainWindow : Window
         CompositionTarget.Rendering += UpdateMangedWindows;
     }
 
-    private void OnWindowAssignLeft(object sender, RoutedEventArgs e)
-    {
-        Button btn = (Button) sender;
-        OnWindowAssign(true);
-    }
+    private void OnWindowAssignLeft(object sender, RoutedEventArgs e) => OnWindowAssign(WindowSlot.Left);
+    private void OnWindowAssignRight(object sender, RoutedEventArgs e) => OnWindowAssign(WindowSlot.Right);
 
-    private void OnWindowAssignRight(object sender, RoutedEventArgs e)
+    private async void OnWindowAssign(WindowSlot position)
     {
-        OnWindowAssign(false);
-    }
-
-    private async void OnWindowAssign(bool isLeft)
-    {
-        IntPtr targetWindow;
-        if (isLeft)
-        {
-            managedLeftWindow = await PollForegroundWindow();
-            targetWindow = (IntPtr) managedLeftWindow;
-        }
-        else
-        {
-            managedRightWindow = await PollForegroundWindow();
-            targetWindow = (IntPtr) managedRightWindow;
-        }
+        IntPtr targetWindow = await PollForegroundWindow();
+        managedWindows[(int)position] = new ManagedWindow(targetWindow);
 
         // grid size
         Console.WriteLine($"Grid Size: {MainGrid.RenderSize.Width}x{MainGrid.RenderSize.Height}");
@@ -109,16 +64,15 @@ public partial class MainWindow : Window
         
         // panel position
         Point panelPosition = gridPosition;
-        if (!isLeft) panelPosition.X += panelSize.Width;
+        if (position == WindowSlot.Right) panelPosition.X += panelSize.Width;
         Console.WriteLine($"Grid Position: {panelPosition.X}x{panelPosition.Y}");
-
-        // remove window borders
-        // -16 means window style changes
-        // 0x00080000 is for no windows styles
-        SetWindowLong(targetWindow, -16, 0x00080000);
         
         // set window position
-        SetWindowPos(targetWindow, WindowInsertAfter.HWND_NOTOPMOST, (int) panelPosition.X, (int) panelPosition.Y, (int) panelSize.Width, (int) panelSize.Height, 0);
+        managedWindows[(int)position].SetPos(
+            (int) panelPosition.X,
+            (int) panelPosition.Y,
+            (int) panelSize.Width,
+            (int) panelSize.Height);
     }
 
     private async Task<IntPtr> PollForegroundWindow()
@@ -130,9 +84,9 @@ public partial class MainWindow : Window
         // poll until window handle is different from ours
         while (foregroundWindow == thisWindowHandle)
         {
-            foregroundWindow = GetForegroundWindow();
             await Task.Delay(TimeSpan.FromMilliseconds(50)); // 20 times per second
-            Console.WriteLine("While");
+            foregroundWindow = GetForegroundWindow();
+            Console.WriteLine($"Polling window handle [{foregroundWindow}]");
         }
 
         return foregroundWindow;
@@ -140,10 +94,13 @@ public partial class MainWindow : Window
 
     private void UpdateMangedWindows(object? sender, EventArgs eventArgs)
     {
-        if (managedLeftWindow is not null) UpdateWindow((IntPtr) managedLeftWindow, true);
-        if (managedRightWindow is not null) UpdateWindow((IntPtr) managedRightWindow, false);
+        for (int i = 0; i < managedWindows.Length; i++)
+        {
+            if (managedWindows[i] is null) continue;
+            UpdateWindow(managedWindows[i], i == 1);
+        }
         
-        void UpdateWindow(IntPtr handle, bool isLeft)
+        void UpdateWindow(ManagedWindow target, bool right)
         {
             // grid position
             Point gridPosition = MainGrid.PointToScreen(new Point(0, 0));
@@ -153,21 +110,19 @@ public partial class MainWindow : Window
         
             // panel position
             Point panelPosition = gridPosition;
-            if (!isLeft) panelPosition.X += panelSize.Width;
+            if (right) panelPosition.X += panelSize.Width;
             
-            SetWindowPos(handle, WindowInsertAfter.HWND_TOPMOST,
+            target.SetPos(
                 (int) panelPosition.X,
-                (int) panelPosition.Y, 
+                (int) panelPosition.Y,
                 (int) panelSize.Width,
-                (int) panelSize.Height,
-                (int) WindowFlags.SWP_NOACTIVATE | (int) WindowFlags.SWP_SHOWWINDOW);
+                (int) panelSize.Height);
         }
     }
 
     private void OnWindowClosing(object? sender, CancelEventArgs e)
     {
         // revert window style changes
-        if (managedLeftWindow is not null) SetWindowLong((IntPtr)managedLeftWindow, -16, 0x00800000 | 0x00C00000 | 0x00080000 | 0x00020000);
-        if (managedRightWindow is not null) SetWindowLong((IntPtr)managedRightWindow, -16, 0x00800000 | 0x00C00000 | 0x00080000 | 0x00020000);
+        foreach (ManagedWindow? window in managedWindows) window?.Dispose();
     }
 }
---
+++ ManagedWindow.cs
@@ -0,0 +1,66 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Split_Schnitzel;
+
+public class ManagedWindow
+{
+    public readonly IntPtr Handle;
+
+    private readonly int previousStyle;
+    private const int WINDOW_STYLE = -16;
+
+    public ManagedWindow(IntPtr handle)
+    {
+        Handle = handle;
+        previousStyle = GetWindowLong(handle, WINDOW_STYLE);
+        SetWindowLong(handle, WINDOW_STYLE, 0x00080000);
+    }
+
+    public void SetPos(int x, int y, int cx, int cy)
+    {
+        SetWindowPos(Handle, WindowInsertAfter.Topmost, x, y, cx, cy, (int) WindowFlags.NoActivate | (int) WindowFlags.ShowWindow);
+    }
+
+    public void Dispose()
+    {
+        SetWindowLong(Handle, WINDOW_STYLE, previousStyle);
+    }
+
+    private enum WindowInsertAfter : int
+    {
+        NoTopmost = -2,
+        Topmost = -1,
+        Top = 0,
+        Bottom = 1
+    }
+
+    private enum WindowFlags : int
+    {
+        AsyncWindowPos = 0x4000,
+        Defererase = 0x2000,
+        DrawFrame = 0x0020,
+        FrameChanged = 0x0020,
+        HideWindow = 0x0080,
+        NoActivate = 0x0010,
+        NoCopyBits = 0x0100,
+        NoMove = 0x0002,
+        NoOwnerZOrder = 0x0200,
+        NoRedraw = 0x0008,
+        NoReposition = 0x0200,
+        NoSendChanging = 0x0400,
+        NoSize = 0x0001,
+        NoZOrder = 0x0004,
+        ShowWindow = 0x0040,
+    }
+    
+    
+    // Window Manipulation Methods
+    [DllImport("user32.dll")]
+    private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    
+    [DllImport("user32.dll")]
+    private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    
+    [DllImport("user32.dll")]
+    private static extern IntPtr SetWindowPos(IntPtr hWnd, WindowInsertAfter hWndInsertAfter, int x, int y, int cx, int cy, int wFlags);
+}
